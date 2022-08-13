efdf782ae91a0259b36d2a13ba623417e4f911da
monees007 <monees007@outlook.com>
2022-08-14 02:47:31 +0530

    Flask temp front end is ready.

--- exchange.py
+++ exchange.py
@@ -5,31 +5,34 @@ from polygon import RESTClient
 from typing import cast
 from urllib3 import HTTPResponse
 import pandas as pd
+def do():
+    client = RESTClient("gcgQPhAjaGgupH0NAIbrce3VxFQdulSL")  # api_key is used
 
-client = RESTClient("gcgQPhAjaGgupH0NAIbrce3VxFQdulSL")  # api_key is used
+    with open("config/input.json") as js:
+        jsn = json.load(js)
 
-with open("config/input.json") as js:
-    jsn = json.load(js)
+    aggs = cast(HTTPResponse,
+                client.get_aggs(
+                    jsn["ticker"][0:-1],
+                    1,
+                    "day",
+                    jsn["from"],
+                    jsn["to"],
+                    raw=False,
+                ),
+                )
 
-dates=(pd.date_range(start="2018-09-09",end="2018-09-18"))
+    f = (aggs.data.decode("utf-8"))
+    box = json.loads(f)
+    print(type(f))
 
-aggs = cast(HTTPResponse,
-            client.get_aggs(
-        jsn["ticker"],
-        1,
-        "day",
-        jsn["from"],
-        jsn["to"],
-        raw=True,
-    ),
-)
+    header = ["v", "vw", "o", "c", "h", "l", "t", "n"]
+    with open("config/output.csv", "w") as cs:
+        writer = csv.DictWriter(cs, fieldnames=header)
+        writer.writeheader()
+        try:
+            writer.writerows(box["results"])
+        except:
+            print(box)
 
-f=((aggs.data.decode("utf-8")))
-box= json.loads(f)
-print(type(f))
-
-header=["v","vw","o","c","h","l","t","n"]
-with open("config/output.csv", "w") as cs:
-    writer=csv.DictWriter(cs, fieldnames = header)
-    writer.writeheader()
-    writer.writerows(box["results"])
+do()
--- templates/app.py
+++
@@ -1,2 +0,0 @@
-import streamlit as st
-st.set_page_config(page_title = "Bazaar : Stock Market Prediction App", page_icon="s", layout="wide")

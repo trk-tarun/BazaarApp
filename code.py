35045ceee3e4cd37f97bd75b5dd5096ac4bfa14e
amber <tarunrajkumar7@gmail.com>
2022-08-14 00:28:37 +0530

    First Commit

--- code.py
+++ code.py
@@ -1,5 +1,17 @@
 import pandas as pd
 
+
+
 # Historical stocks price dataset
 datafile = pd.read_csv('Dataset\dataset2.csv')
-print(datafile)
+
+#print(datafile) #test datafile
+
+#summary of dataset
+
+print(datafile.describe())
+
+#Objective 1: Using Linear Regression
+
+#reindexing using DateTimeIndex
+datafile.set_index(pd.DatetimeIndex(datafile['Date']),inplace=True)

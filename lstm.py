8c1e6cb3f55776e72108a7ba705689e2206f0a96
amber <tarunrajkumar7@gmail.com>
2022-08-14 14:09:40 +0530

    Update Commit

--- lstm.py
+++ lstm.py
@@ -15,4 +15,38 @@ trainData = data.iloc[:, 4:5].values
 
 data.info()
 
-sc =
+sc = MinMaxScaler(feature_range=(0,1))
+trainData = sc.fit_transform(trainData)\
+trainData.shape
+
+###
+
+X_train = []
+Y_train = []
+
+#60 is time step  ## 1149 = length of data
+
+for i in range (60, 1149):
+    X_train.append(trainData[i-60:i, 0])
+    Y_train.append(trainData[i,0])
+
+X_train,Y_train = np.array(X_train), np.array(Y_train)
+
+X_train = np.reshape(X_train,(X_train.shape[0], X_train.shape[1],1))
+X_train.shape
+model = Sequential()
+
+model.add(LSTM(units=100, return_sequences=True, input_shape=(X_train.shape[1],1)))
+model.add(Dropout(0.2))
+
+model.add(LSTM(units=100, return_sequences=True))
+model.add(Dropout(0.2))
+
+model.add(LSTM(units=100, return_sequences=True))
+model.add(Dropout(0.2))
+
+model.add(LSTM(units=100, return_sequences=True))
+model.add(Dropout(0.2))
+
+model.add(Dense(units=1))
+model.compile(optimizer='adam', loss="mean_squared_error")

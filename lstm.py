76f261d09ee6b5f29def0084473dbed81179cbde
amber <tarunrajkumar7@gmail.com>
2022-08-14 13:53:31 +0530

    Update Commit

--- lstm.py
+++ lstm.py
@@ -4,3 +4,15 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense,LSTM,Dropout
+
+#training dataset
+
+data = pd.read_csv('Dataset2/apple_train_dataset.csv')
+data.info
+data["Close"] = pd.to_numeric(data.Close,errors='coerce')
+data=data.dropna()
+trainData = data.iloc[:, 4:5].values
+
+data.info()
+
+sc =

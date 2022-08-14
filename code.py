494577194294edceaeabac0a4765a1ee134c0f5d
amber <tarunrajkumar7@gmail.com>
2022-08-14 05:30:00 +0530

    Update Commit

--- code.py
+++ code.py
@@ -22,19 +22,22 @@ datafile.info()
 datafile['Date'] = pd.to_datetime(datafile['Date'])
 
 print(f'Dataframe between{datafile.Date.min()}{datafile.Date.max()}')
+print(f'Total Days = {(datafile.Date.max() - datafile.Date.min()).days} days')
 #summary of dataset
 
-print(datafile.describe())
+#print(datafile.describe())
+
+datafile[['Open', 'High', 'Low', 'Close', 'Adj Close']].plot(kind='box')
 
 #Objective 1: Using Linear Regression
 
 
 
-datafile = datafile[['Adj Close']]
-
-print(datafile)
-
-#printing info
-print(datafile.info())
+# datafile = datafile[['Adj Close']]
+#
+# print(datafile)
+#
+# #printing info
+# print(datafile.info())
 
 
